c45e6c7aadfd20955e586dddbdd3477a4fe9ba6c
charliemolony <charliemolony59@gmail.com>
2024-03-12 17:50:25 +0000

    again

--- src/tasks/task_util.py
+++ src/tasks/task_util.py
@@ -33,9 +33,8 @@ def init_replay_buffer(env, model, time_window,dataset_path, n_episodes=1000,tas
 
     dataset_file_path='/content/ITERS2.0/datasets/highway/data.pkl'
     try:
-        with open(dataset_file_path, 'rb') as file:
-            dataset = pickle.load(file)    
 
+        dataset = torch.load(dataset_file_path)
     except Exception as e:
         print('Initializing replay buffer with env reward...')
 
